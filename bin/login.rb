8ffa29c6bf3643c4c966555ba02bfa61f47590f5
tayutaedomo <tayutaedomo@gmail.com>
2016-07-30 18:18:40 +0900

    Add comment to login script

--- bin/login.rb
+++ bin/login.rb
@@ -2,6 +2,8 @@
 
 require 'spaceship'
 
+# Refer: https://github.com/fastlane/fastlane/blob/master/spaceship/docs/DeveloperPortal.md#login
+
 Spaceship.login(ENV['SPACESHIP_MAIL'], ENV['SPACESHIP_PASS'])
 
 p Spaceship.select_team
